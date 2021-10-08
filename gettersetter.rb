c957dab8a79cdffb3897c524ca4415e45011862d
Danny Barrientos <danny.barrientos@blackboard.com>
2021-10-07 23:20:02 -0500

    Objetos setter getter con metodos de ruby

--- gettersetter.rb
+++ gettersetter.rb
@@ -21,8 +21,8 @@ puts danny.get_nombre
 
 =end
 
-#Asi seria en ruby
 
+=begin #Asi seria en ruby
 class Profesor
     def initialize(nombre)
         @nombre = nombre #propiedad
@@ -41,3 +41,23 @@ danny = Profesor.new("Danny")
 danny.nombre= "Andres"
 
 puts danny.nombre
+=end 
+
+#Asi seria con ruby con metodos de ruby
+class Profesor
+
+    #attr_accessor  -Setter -getter
+    #attr_reader  Getter
+    #attr_writer  setter
+
+    attr_accessor :nombre, :edad
+    def initialize(nombre, edad)
+        @nombre = nombre
+        @edad = edad
+    end  
+end
+danny = Profesor.new("Danny", 24)
+danny.nombre= "Andres"
+danny.edad = 20
+puts danny.nombre
+puts danny.edad
