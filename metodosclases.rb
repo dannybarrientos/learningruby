3f883b9fac5d9bf8e58e86ddc13f0f1abf721cdf
Danny Barrientos <danny.barrientos@blackboard.com>
2021-10-10 18:36:40 -0500

    metodos de clases

--- metodosclases.rb
+++ metodosclases.rb
@@ -1,17 +1,26 @@
+#Metodos de clases se utilizan cuando la funcionalidad no le pertenece a una instancia o objeto
+
 class Carro
     @marca = 'Renaul'
-
-    def marca
-        @marca
+   
+    def arrancar
+    puts "Arrancandoo!!!"
     end
 
-    def marca=(marca)
-        @marca = marca
-
-    end    
+    class << self
+        def marca
+            @marca
+        end
+    
+        def marca=(marca)
+            @marca = marca
+        end 
+    end
     
 end
 
-rolex = Carro.new
-rolex.marca = 'Rolex'
-puts rolex.marca
+Carro.marca = 'Nissan'
+puts Carro.marca
+
+bww =Carro.new
+bww.arrancar
